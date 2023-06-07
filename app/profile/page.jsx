61c3c72a5b261de722a4abee696ba1d9cf7545e2
Ridwan Hidayat <ridwan.nurulhidayat@gmail.com>
2023-06-07 07:25:23 +0700

    deployment

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,43 +1,60 @@
-'use client';
+"use client";
 
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
-import { useState, useEffect } from 'react';
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
 
-import Profile from '@components/Profile';
+import Profile from "@components/Profile";
 
 const MyProfile = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
 
-    const router = useRouter();
-    const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
 
-    const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+      setPosts(data);
+    };
 
-    useEffect(() => {
-        const fetchPosts = async () => {
-            const response = await fetch(`/api/users/${session?.user.id}/posts`);
-            const data = await response.json();
-            setPosts(data);
-        }
+    if (session?.user.id) fetchPosts();
+  }, []);
 
-        if (session?.user.id) fetchPosts();
+  const handleEdit = async (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
 
-    }, []);
-
-    const handleEdit = async (post) => {
-        router.push(`/update-prompt?id=${post._id}`);
-    }
-    const handleDelete = async () => { }
-
-    return (
-        <Profile
-            name='My'
-            desc='Welcome to my profile page'
-            data={posts}
-            handleEdit={handleEdit}
-            handleDelete={handleDelete}
-        />
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
     );
-}
 
-export default MyProfile
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPosts = posts.filter((p) => p._id !== post._id);
+
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to my profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
+};
+
+export default MyProfile;
